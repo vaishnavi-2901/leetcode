10b2721b46728359a6d89ebec96125be5051f48a
vaishnavi pandey <36892963+vishu1089@users.noreply.github.com>
2023-01-08 20:37:35 +0530

    Create topo_sort.cpp
    https://practice.geeksforgeeks.org/problems/topological-sort/

---
+++ topo_sort.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    vector<int> res, topo;
+	    for(int i=0;i<V;i++) res.push_back(0);
+	    for(int i=0;i<V;i++)
+	        for(auto e:adj[i]) 
+	            res[e]++;
+	    queue<int> q;
+	    for(int i=0;i<V;i++) if(res[i]==0) q.push(i); //sorting according to indegree
+	    while(!q.empty()){
+	        int temp = q.front();
+	        q.pop();
+	        topo.push_back(temp);
+	        for(auto e : adj[temp]){
+	            res[e]--;
+	            if(res[e]==0) q.push(e);
+	        }
+	    }
+	    return topo;
+	}
+};
+
+//{ Driver Code Starts.
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    
+    if(V!=res.size())
+    return 0;
+    
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++) {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        
+        Solution obj;
+        vector <int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
