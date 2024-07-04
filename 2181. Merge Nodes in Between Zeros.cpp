f9557c2d9e7c618675cc26e86d2d1f2a788b6766
vaishnavi pandey <36892963+vaishnavi-2901@users.noreply.github.com>
2024-07-04 22:31:45 +0530

    Create 2181. Merge Nodes in Between Zeros.cpp

---
+++ 2181. Merge Nodes in Between Zeros.cpp
@@ -0,0 +1,76 @@
+/**
+
+
+
+You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
+
+For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. The modified list should not contain any 0's.
+
+Return the head of the modified linked list.
+
+ 
+
+Example 1:
+
+
+Input: head = [0,3,1,0,4,5,2,0]
+Output: [4,11]
+Explanation: 
+The above figure represents the given linked list. The modified list contains
+- The sum of the nodes marked in green: 3 + 1 = 4.
+- The sum of the nodes marked in red: 4 + 5 + 2 = 11.
+Example 2:
+
+
+Input: head = [0,1,0,3,0,2,2,0]
+Output: [1,3,4]
+Explanation: 
+The above figure represents the given linked list. The modified list contains
+- The sum of the nodes marked in green: 1 = 1.
+- The sum of the nodes marked in red: 3 = 3.
+- The sum of the nodes marked in yellow: 2 + 2 = 4.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [3, 2 * 105].
+0 <= Node.val <= 1000
+There are no two consecutive nodes with Node.val == 0.
+The beginning and end of the linked list have Node.val == 0.
+
+
+
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode dummy(0); // Use a dummy node to simplify edge cases
+        ListNode* res = &dummy; // Start res at the dummy node
+        int sum = 0;
+        
+        head = head->next; // Skip the initial zero as per the problem statement
+        
+        while (head) {
+            if (head->val != 0) {
+                sum += head->val;
+            } else {
+                if (sum != 0) {
+                    res->next = new ListNode(sum); // Create a new node with the sum
+                    res = res->next; // Move to the new node
+                    sum = 0; // Reset the sum for the next segment
+                }
+            }
+            head = head->next; // Move to the next node in the input list
+        }
+        
+        return dummy.next; // Return the head of the merged nodes list
+    }
+};
